f82bceb731837f2da2965aefe2ce6fb30da71384
Lera <vorjnmaaa666@gmail.com>
2024-10-25 14:20:46 +0500

    1

--- Lab2/Weather.cs
+++ Lab2/Weather.cs
@@ -48,39 +48,4 @@ namespace Lab2
             return $"Температура: {temperature}°C, Влажность: {humidity}%, Скорость ветра: {windSpeed} км/ч";
         }
     }
-
-    public class WeatherForecast : Weather
-    {
-        private int tomorrowTemp;
-        private int dayAfterTomorrowTemp;
-
-        public WeatherForecast(int temperature, int humidity, int windSpeed, int tomorrowTemp, int dayAfterTomorrowTemp) : base(temperature, humidity, windSpeed) // Конструктор для создания прогноза погоды
-        {
-            this.tomorrowTemp = tomorrowTemp;
-            this.dayAfterTomorrowTemp = dayAfterTomorrowTemp;
-        }
-
-        public WeatherForecast(WeatherForecast other) : base(other)// Конструктор копирования
-        {
-            this.tomorrowTemp = other.tomorrowTemp;
-            this.dayAfterTomorrowTemp = other.dayAfterTomorrowTemp;
-        }
-
-        public bool IsTomorrowWarmerThanToday() // Метод для сравнения средней температуры
-        {
-            return tomorrowTemp > this.Temperature;
-        }
-
-        public double GetAverageTemperature() // Метод для вычисления средней температуры
-        {
-            return (this.Temperature + tomorrowTemp + dayAfterTomorrowTemp) / 3.0;
-        }
-
-
-        public override string ToString() // Метод ToString для формирования строки с информацией о погоде и прогнозом
-        {
-            return base.ToString() + $"\nСегодня: {tomorrowTemp}°C, Послезавтра: {dayAfterTomorrowTemp}°C";
-        }
-    }
-
 }
